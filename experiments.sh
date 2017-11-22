1f2a2b84e85be222aae49bc749e60f1f65beb228
Jason Alan Fries <fries.jason@gmail.com>
2017-11-21 19:26:30 -0800

    typo in experiments.sh resulted in loading the wrong top5 parameter grids for the spouse dataset

--- experiments.sh
+++ experiments.sh
@@ -5,16 +5,16 @@
 #
 python benchmark.py -d cdr-supervised -c configs/pca.json -g configs/top5/cdr-supervised_pca.json -E 200 > cdr-supervised_pca.log 2> cdr-supervised_pca.err
 python benchmark.py -d cdr-dp -c configs/pca.json -g configs/top5/cdr-supervised_pca.json -E 200 > cdr-dp_pca.log 2> cdr-dp_pca.err
-python benchmark.py -d spouse-supervised -c configs/pca.json -g configs/top5/cdr-supervised_pca.json -E 200 > spouse-supervised_pca.log 2> spouse-supervised_pca.err
-python benchmark.py -d spouse-dp -c configs/pca.json -g configs/top5/cdr-supervised_pca.json -E 200 > spouse-dp_pca.log 2> spouse-dp_pca.err
+python benchmark.py -d spouse-supervised -c configs/pca.json -g configs/top5/spouse-supervised_pca.json -E 200 > spouse-supervised_pca.log 2> spouse-supervised_pca.err
+python benchmark.py -d spouse-dp -c configs/pca.json -g configs/top5/spouse-dp_pca.json -E 200 > spouse-dp_pca.log 2> spouse-dp_pca.err
 
 #
 # PCA + Exp. Decay Kernel
 #
 python benchmark.py -d cdr-supervised -c configs/pca-kernel.json -g configs/top5/cdr-supervised_pca-kernel.json -E 200 > cdr-supervised_pca-kernel.log 2> cdr-supervised_pca-kernel.err
 python benchmark.py -d cdr-dp -c configs/pca-kernel.json -g configs/top5/cdr-supervised_pca-kernel.json -E 200  > cdr-dp_pca-kernel.log 2> cdr-dp_pca-kernel.err
-python benchmark.py -d spouse-supervised -c configs/pca-kernel.json -g configs/top5/cdr-supervised_pca-kernel.json -E 200 > spouse-supervised_pca-kernel.log 2> spouse-supervised_pca-kernel.err
-python benchmark.py -d spouse-dp -c configs/pca-kernel.json -g configs/top5/cdr-supervised_pca-kernel.json -E 200 > spouse-dp_pca-kernel.log 2> spouse-dp_pca-kernel.err
+python benchmark.py -d spouse-supervised -c configs/pca-kernel.json -g configs/top5/spouse-supervised_pca-kernel.json -E 200 > spouse-supervised_pca-kernel.log 2> spouse-supervised_pca-kernel.err
+python benchmark.py -d spouse-dp -c configs/pca-kernel.json -g configs/top5/spouse-dp_pca-kernel.json -E 200 > spouse-dp_pca-kernel.log 2> spouse-dp_pca-kernel.err
 
 #
 # LSTM
